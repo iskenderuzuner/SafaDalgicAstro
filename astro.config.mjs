6cbc96f4a0e3cb67a42318c2eec93b106cb319ee
Iskender Uzuner <iskenderuzuner@gmail.com>
2026-01-27 22:00:14 +0300

    hizmetler icerik foto16

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
-import markdoc from '@astrojs/markdoc'; // Yeni ekledik
+import markdoc from '@astrojs/markdoc';
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap'; // 1. EKLENEN: Sitemap importu
 
 export default defineConfig({
+  site: 'https://safadalgicpompa.com', // 2. EKLENEN: Site adresin (Sitemap için şart)
   output: 'hybrid',
   adapter: vercel(),
   integrations: [
     react(),
     keystatic(),
-    markdoc() // Yeni ekledik
+    markdoc(),
+    sitemap() // 3. EKLENEN: Entegrasyon listesine ekledik
   ]
 });
