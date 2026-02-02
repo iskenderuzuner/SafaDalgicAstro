6792a95e86e7339b4675eae62e8bf67530c8ca39
Iskender Uzuner <iskenderuzuner@gmail.com>
2026-02-02 15:29:51 +0300

    tablo duzenlemesi2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import markdoc from '@astrojs/markdoc';
 import vercel from '@astrojs/vercel/serverless';
+import remarkGfm from 'remark-gfm';
 // import sitemap iptal edildi
 
 export default defineConfig({
   site: 'https://safadalgicpompa.com',
   output: 'hybrid',
   adapter: vercel(),
+  markdown: {
+    remarkPlugins: [remarkGfm],
+  },
   integrations: [
     react(),
     keystatic(),
